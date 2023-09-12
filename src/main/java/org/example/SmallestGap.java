8da0df752e4572522dc72c8fe6aab6f6ffd5f6ea
Onni Virtanen <ac7545@student.jamk.fi>
2023-09-12 12:59:34 +0300

    refactored

--- src/main/java/org/example/SmallestGap.java
+++ src/main/java/org/example/SmallestGap.java
@@ -5,28 +5,32 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SmallestGap {
-    public static Integer findSmallestGap(List<Integer> integers) {
+    public static Integer findSmallestGap(final List<Integer> integers) {
         validateList(integers);
-        List<Integer> sortedList = sortListAndRemoveDuplicates(integers);
+        List<Integer> uniqueIntegers = removeDuplicates(integers);
+        List<Integer> sortedList = sortList(uniqueIntegers);
         return findSmallestGapFromSortedList(sortedList);
     }
 
-    private static List<Integer> sortListAndRemoveDuplicates(List<Integer> integersWithPossibleDuplicates) {
-        Set<Integer> set = new HashSet<>(integersWithPossibleDuplicates);
-        List<Integer> integersWithoutDuplicates = new ArrayList<>(set);
-        Collections.sort(integersWithoutDuplicates);
-        return integersWithoutDuplicates;
-    }
-
-    private static void validateList(List<Integer> integers) {
+    private static void validateList(final List<Integer> integers) {
         if (integers.isEmpty() || integers.size() == 1) {
             throw new IllegalArgumentException("A list with minimum of two elements should be provided.");
         }
     }
 
-    private static Integer findSmallestGapFromSortedList(List<Integer> integers) {
+    private static List<Integer> removeDuplicates(final List<Integer> integers) {
+        Set<Integer> set = new HashSet<>(integers);
+        return new ArrayList<>(set);
+    }
+
+    private static List<Integer> sortList(final List<Integer> integers) {
+        return integers.stream().sorted().collect(Collectors.toList());
+    }
+
+    private static Integer findSmallestGapFromSortedList(final List<Integer> integers) {
         int smallestNumberGap = Integer.MAX_VALUE;
         for (int i = 1; i < integers.size(); i++) {
             int currentInterval = integers.get(i) - integers.get(i - 1) - 1;
